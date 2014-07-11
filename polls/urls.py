48caccf31f7fd84d4faa5a6ec14f545ae5b62c40
clayton miller <pimpc@claytonmiller.com>
2014-07-11 17:00:59 -0500

    changes template names and cleaned up classes

--- polls/urls.py
+++ polls/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url
 
-from polls import views
+from polls import views	
 
 urlpatterns = [
-	url(r'^$', views.IndexView.as_view(), name='index'),
-	url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
-	url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
-	url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
+	url(r'^$', views.IndexView.as_view(), name='polls-index'),
+	url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='polls-detail'),
+	url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='polls-results'),
+	url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='polls-vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -6,22 +6,15 @@ from django.views import generic
 from polls.models import Choice, Poll
 
 class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_poll_list'
-
     def get_queryset(self):
-        """Return the last five published polls."""
         return Poll.objects.order_by('-pub_date')[:5]
-
 
 class DetailView(generic.DetailView):
     model = Poll
-    template_name = 'polls/detail.html'
-
 
 class ResultsView(generic.DetailView):
     model = Poll
-    template_name = 'polls/results.html'
+    template_name = 'polls/poll_results.html'
 
 
 def vote(request, poll_id):
@@ -30,7 +23,7 @@ def vote(request, poll_id):
         selected_choice = p.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the poll voting form.
-        return render(request, 'polls/detail.html', {
+        return render(request, 'polls/poll_detail.html', {
             'poll': p,
             'error_message': "You didn't select a choice.",
         })
@@ -40,4 +33,4 @@ def vote(request, poll_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
+        return HttpResponseRedirect(reverse('polls-results', args=(p.id,)))

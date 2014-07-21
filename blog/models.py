d150c63fe868c35f8de45608b77927456b892a4d
clayton miller <pimpc@claytonmiller.com>
2014-07-21 15:08:52 -0500

    added photo to the post model

--- blog/models.py
+++ blog/models.py
@@ -1,22 +1,20 @@
+from django.core.urlresolvers import reverse
 from django.db import models
 
-from django.core.urlresolvers import reverse
 
-# Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(unique=True, max_length=255)
     content = models.TextField()
+    photo = models.ImageField(upload_to='images/photos/')
     published = models.BooleanField(default=True)
     created = models.DateTimeField('auto_add_now=True')
 
     class Meta:
         ordering = ['-created']
-        
 
     def __unicode__(self):
-    	return self.title
-
+        return self.title
 
     def get_absolute_url(self):
         return reverse('blog-detail', args=[self.slug])

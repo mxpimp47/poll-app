04bee14151d7478c8060c9e16f22ae153290bde5
clayton miller <pimpc@claytonmiller.com>
2014-07-21 15:02:43 -0500

    fixed indentation

--- polls/models.py
+++ polls/models.py
@@ -2,19 +2,20 @@ import datetime
 from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+
 class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-    def __unicode__(self): 
-    	return self.question
+    def __unicode__(self):
+        return self.question
 
     def was_published_recently(self):
-    	return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-    was_published_recently.admin_order_field = 'pub_date'
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     was_published_recently.boolean = True
+    was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.short_description = 'Published recently?'
+
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
@@ -22,4 +23,4 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
 
     def __unicode__(self):
-    	return self.choice_text
+        return self.choice_text

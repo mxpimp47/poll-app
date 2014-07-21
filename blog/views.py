44555368c2a6f29a6adf6912ff4479f09df7142f
clayton miller <pimpc@claytonmiller.com>
2014-07-21 15:06:45 -0500

    removed comments

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-from blog.models import Post
-     
+
+from .models import Post
+
+
 def index(request):
-    # get the blog posts that are published
-    posts = Post.objects.filter(published=True)
-    # now return the rendered template
+    posts = Post.objects.filter(published=True)  # published posts
     return render(request, 'blog/index.html', {'posts': posts})
-     
+
+
 def post(request, slug):
-    # get the Post object
-    post = get_object_or_404(Post, slug=slug)
-    # now return the rendered template
+    post = get_object_or_404(Post, slug=slug)  # get post
     return render(request, 'blog/post.html', {'post': post})

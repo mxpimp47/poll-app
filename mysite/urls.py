091404de4c96074cb6f4953531de10e35e3a3e38
clayton miller <pimpc@claytonmiller.com>
2014-07-21 15:03:55 -0500

    added url for images

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
-
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -8,4 +9,4 @@ urlpatterns = [
     url(r'^newsletter/', include('newsletter.urls')),
     url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
